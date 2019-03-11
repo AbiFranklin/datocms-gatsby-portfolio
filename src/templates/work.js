d9afe4df4c056e54cfad4e14c6965ee4df35caa6
Abi <abifranklin@gmail.com>
2019-03-11 12:16:54 -0500

    Added cover image back in for index

--- src/templates/work.js
+++ src/templates/work.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Layout from "../components/layout"
 
 export default ({ data }) => (

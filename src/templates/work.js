78b9159e9b70fc86d0c3d0d30fc6ab6da0d3d2a4
Abi <abifranklin@gmail.com>
2019-03-11 12:13:13 -0500

    Removed images from posts

--- src/templates/work.js
+++ src/templates/work.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import Slider from 'react-slick'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
-import Img from 'gatsby-image'
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 
@@ -12,22 +10,12 @@ export default ({ data }) => (
       <div className="sheet__inner">
         <h1 className="sheet__title">{data.datoCmsWork.title}</h1>
         <p className="sheet__lead">{data.datoCmsWork.excerpt}</p>
-        <div className="sheet__slider">
-          <Slider infinite={true} slidesToShow={2} arrows>
-            {data.datoCmsWork.gallery.map(({ fluid }) => (
-              <img alt={data.datoCmsWork.title} key={fluid.src} src={fluid.src} />
-            ))}
-          </Slider>
-        </div>
         <div
           className="sheet__body"
           dangerouslySetInnerHTML={{
             __html: data.datoCmsWork.descriptionNode.childMarkdownRemark.html,
           }}
         />
-        <div className="sheet__gallery">
-          <Img fluid={data.datoCmsWork.coverImage.fluid} />
-        </div>
       </div>
     </article>
   </Layout>
@@ -41,20 +29,9 @@ export const query = graphql`
       }
       title
       excerpt
-      gallery {
-        fluid(maxWidth: 200, imgixParams: { fm: "jpg", auto: "compress" }) {
-          src
-        }
-      }
       descriptionNode {
         childMarkdownRemark {
           html
-        }
-      }
-      coverImage {
-        url
-        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
-          ...GatsbyDatoCmsSizes
         }
       }
     }
